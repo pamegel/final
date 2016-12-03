adc5ccc7a7843e3a86328acf5f8f39fa4fbeb79e
pamegel <pamezeza2@gmail.com>
2016-12-03 23:09:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ break;*/
 } else if (messageAttachments) {
   sendTextMessage(senderID, "จุ๊บๆ");
 }
-}
+
 function receivedPostback(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
@@ -164,6 +164,7 @@ function receivedPostback(event) {
           }
 }
    ///////////////////////////////////////////////////////////////////////////////////////////////
+   }
 
   // When a postback is called, we'll send a message back to the sender to
   // let them know it was successful
