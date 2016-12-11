d65827c5f75724489278738e2f051e8d39925380
pamegel <pamezeza2@gmail.com>
2016-12-11 11:30:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -73,22 +73,55 @@ function sendTextMessage (sender, text) {
   })
 }
 
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: token},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: messageData
+function sendGenericMessage (sender) {
+let messageData = {
+  'attachment': {
+    'type': 'template',
+    'payload': {
+      'template_type': 'generic',
+      'elements': [{
+        'title': 'First card',
+        'subtitle': 'Element #1 of an hscroll',
+        'image_url': 'http://messengerdemo.parseapp.com/img/rift.png',
+        'buttons': [{
+          'type': 'web_url',
+          'url': 'https://www.messenger.com',
+          'title': 'web url'
+        }, {
+          'type': 'postback',
+          'title': 'Postback',
+          'payload': 'Payload for first element in a generic bubble'
+        }]
+      }, {
+        'title': 'Second card',
+        'subtitle': 'Element #2 of an hscroll',
+        'image_url': 'http://messengerdemo.parseapp.com/img/gearvr.png',
+        'buttons': [{
+          'type': 'postback',
+          'title': 'Postback',
+          'payload': 'Payload for second element in a generic bubble'
+        }]
+      }]
     }
-  }, function (error, response, body) {
-    if (error) {
-      console.log('Error sending messages: ', error)
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error)
-    }
-  })
+  }
+}
+request({
+  url: 'https://graph.facebook.com/v2.6/me/messages',
+  qs: {access_token: token},
+  method: 'POST',
+  json: {
+    recipient: {id: sender},
+    message: messageData
+  }
+}, function (error, response, body) {
+  if (error) {
+    console.log('Error sending messages: ', error)
+  } else if (response.body.error) {
+    console.log('Error: ', response.body.error)
+  }
+})
+}
 
 app.listen(app.get('port'), function () {
-  console.log('running on port', app.get('port'))
+console.log('running on port', app.get('port'))
 })
